70bf839686528f26af948f3a575308a892643f12
hdlam <instagibno@gmail.com>
2014-01-24 10:40:28 +0100

    syncing code

--- assignment1/robots/swarm-epuck/controllers/swarm_controller/swarm_controller.py
+++ assignment1/robots/swarm-epuck/controllers/swarm_controller/swarm_controller.py
@@ -30,17 +30,17 @@ def search():
 	if(sum(left)/len(left)>colorThresh):
 		if(sum(right)/len(right)>colorThresh):
 			##this makes them converge after a while
-			# if(distances[0] > 500 and distances[7] >500):
-			# 	if(distances[0]>distances[7]):
-			# 		controller.spin_right(0.5,0.1)
-			# 	elif(distances[0]<distances[7]):
-			# 		controller.spin_left(0.5,0.1)
-			# 	else:
-			# 		update_search_speed(controller.get_proximities(), distance_thresh)
-			# 		controller.move_wheels(get_search_left_wheel_speed(), get_search_right_wheel_speed(), 0.1)
-			# else:
-			retr = True
-			controller.led[8].set(1)
+			if(distances[0] < 1000 and distances[7] <1000):
+				if(distances[0]>distances[7]):
+					controller.spin_right(0.5,0.1)
+				elif(distances[0]<distances[7]):
+					controller.spin_left(0.5,0.1)
+				else:
+					update_search_speed(controller.get_proximities(), distance_thresh)
+					controller.move_wheels(get_search_left_wheel_speed(), get_search_right_wheel_speed(), 0.1)
+			else:
+				retr = True
+				controller.led[8].set(1)
 		else:
 			controller.spin_left(1,0.1)
 	elif(sum(right)/len(right)>colorThresh):
@@ -54,19 +54,22 @@ def search():
 
 
 def retrieval():
-	image = controller.get_image()
-	#left = imageAvg[0:len(imageAvg)/2]
-	#right = imageAvg[len(imageAvg)/2:len(imageAvg)]
+	imageAvg = imageArrayAvg(controller.camera.getImageArray())
+	left = imageAvg[0:5]
+	right = imageAvg[len(imageAvg)-5:len(imageAvg)]
 	colorThresh = 200
 	distances = controller.get_proximities()
 	
-	if(image_avg(image) < colorThresh):
-		retr = False
-		
-	irThresh = 100
-	distances = controller.get_proximities()
+	#print str(sum(left)/len(left))+ "+" + str(sum(right)/len(right))
+
+	#print imageAvg
+
+
+	# if(sum(left)/len(left)>colorThresh):
+	# 	if(sum(right)/len(right)>colorThresh):
+
 	select_behavior(distances)
-	swarm_retrieval(distances, irThresh)
+	swarm_retrieval(distances, 200)
 	controller.move_wheels(get_retrieval_left_wheel_speed, get_retrieval_right_wheel_speed(), 0.1)
 
 def stagnation():
@@ -77,20 +80,19 @@ def stagnation():
 
 controller = EpuckBasic()
 controller.basic_setup()
-controller.move(0.1)
 #for i in range(0,2):
 while(True):
-	#print column_avg(controller.get_image())[0]
-	#controller.move()
-	#print get_red_image(controller.get_image())
-	#print controller.camera.getImageArray()[0]
-	#print imageArrayAvg(controller.camera.getImageArray())
-	# if(stag):
-	# 	stagnation()
-	# elif(retr):
-	# 	retrieval()
-	# else:
-	search()
+	if(stag):
+		stagnation()
+		#retrieve:
+	distances = controller.get_proximities()
+	select_behavior(distances)
+	swarm_retrieval(distances, 200)
+	controller.move_wheels(get_retrieval_left_wheel_speed(), get_retrieval_right_wheel_speed(), 0.1)
+	if(get_retrieval_left_wheel_speed() == 0 and get_retrieval_right_wheel_speed() == 0):
+		controller.move()
+	
+	
 
 controller.stop_moving()
 
