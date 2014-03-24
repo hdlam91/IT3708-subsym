98692ec0ba4acb147479b65ce4487a1278953802
hdlam <instagibno@gmail.com>
2014-03-24 14:08:37 +0100

    added support for double v

--- Flatland/src/simpleslickgame/ANN.java
+++ Flatland/src/simpleslickgame/ANN.java
@@ -1,11 +1,14 @@
 package simpleslickgame;
 
+import java.util.Arrays;
+
 public class ANN {
 	
 	private Node foodInputLeft, foodInputFront, foodInputRight;
 	private Node poisonInputLeft, poisonInputFront, poisonInputRight;
 	private Node leftOutput, frontOutput, rightOutput;
-	
+	private Node[] weights;
+	int numberOfNode;
 	public ANN() {
 		foodInputLeft = new Node();
 		foodInputFront = new Node();
@@ -14,10 +17,42 @@ public class ANN {
 		poisonInputFront = new Node();
 		poisonInputRight = new Node();
 		
+		numberOfNode = 6;
+		
 		leftOutput = new Node();
 		frontOutput = new Node();
 		rightOutput = new Node();
 		
+		weights = new Node[3];
+		weights[0] = leftOutput;
+		weights[1] = frontOutput;
+		weights[2] = rightOutput;
+		
+	}
+	
+	public void test(){
+		for (int i = 0; i < weights.length; i++) {
+			System.out.println(weights[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		ANN a = new ANN();
+		double v[] = new double[30];
+		for (int i = 0; i < v.length; i++) {
+			v[i] = i;
+		}
+		a.setWeight(v);
+		a.test();
+	}
+	
+	public void setWeight(double v[]){
+		 double v1[] = Arrays.copyOfRange(v, 0, v.length/weights.length);
+		 double v2[] = Arrays.copyOfRange(v, v.length/weights.length, (v.length/weights.length)*2);
+		 double v3[] = Arrays.copyOfRange(v, (v.length/weights.length)*2, (v.length));
+		 weights[0].setWeight(v1);
+		 weights[1].setWeight(v2);
+		 weights[2].setWeight(v3);
 		
 	}
 	
@@ -25,8 +60,10 @@ public class ANN {
 	
 	
 	public void input(boolean fleft, boolean ffront, boolean fright,boolean pleft, boolean pfront, boolean pright){
-		//DO something
-		
+		//DO something		
+		foodInputLeft.input(fleft);
+		foodInputFront.input(ffront);
+		foodInputRight.input(fright);
 		
 	}
 	
