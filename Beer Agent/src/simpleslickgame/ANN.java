edb2309a7e8d4724b77eb16595d879aea645f21b
hdlam <instagibno@gmail.com>
2014-04-01 16:36:48 +0200

    more ANN

--- Beer Agent/src/simpleslickgame/ANN.java
+++ Beer Agent/src/simpleslickgame/ANN.java
@@ -1,5 +1,6 @@
 package simpleslickgame;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -80,9 +81,10 @@ public class ANN {
 
 
  public static void main(String[] args) {
-        int [] li = {3};
+        int [] li = {2};
         ANN a = new ANN(li,0.5,5);
-        double v[] = new double[30];
+        int needed = a.getNumberOfNodes()*3+a.getNumberOfWeightsNeeded();
+        double v[] = new double[needed];
         for (int i = 0; i < v.length; i++) {
             v[i] = i;
         }   
@@ -101,29 +103,54 @@ public class ANN {
 			
  	}
 	
+ 	
  
- public int getNumberOfWeightsNeeded(double v[]){
-		int numOfW = 0;
-		if(hiddenLayerUsed){
-			numOfW += numOfInput*hiddenLayerStructure[0];
-			for (int i = 1; i < hiddenLayerStructure.length; i++) {
-				numOfW += hiddenLayerStructure[i-1]*hiddenLayerStructure[i];
-			}
-			for (int i = 0; i < hiddenLayerStructure.length; i++) {
-				numOfW += hiddenLayerStructure[i]; //bias weight
-				numOfW += hiddenLayerStructure[i]*hiddenLayerStructure[i]; //self weight
-			}
-			numOfW += numOfOutput*hiddenLayerStructure[hiddenLayerStructure.length-1]+numOfOutput;
-		}
-		else{
-			numOfW = numOfInput*numOfOutput+numOfOutput;
-		}
-		numOfW+= numOfOutput*numOfOutput;
-		
-		System.out.println(hiddenLayerStructure[0]);
-		return numOfW;
-	}
-
+ 	public void setBias(double b[]){
+ 		int counter = 0;
+ 		if(hiddenLayerUsed){
+	 		for (int i = 0; i < hiddenLayerStructure.length; i++) {
+	 			for (int j = 0; j < hiddenLayerStructure[i]; j++) {
+					hiddenNodes.get(counter).setBiasWeight(b[counter]);
+					counter++;
+				}
+	 		}
+ 		}
+ 		for (int i = 0; i < numOfOutput; i++) {
+ 			outputs.get(i).setBiasWeight(b[counter]);
+ 			counter++;
+ 		}
+ 	}
+ 	
+ 	public void setGains(double g[]){
+ 		int counter = 0;
+ 		if(hiddenLayerUsed){
+	 		for (int i = 0; i < hiddenLayerStructure.length; i++) {
+	 			for (int j = 0; j < hiddenLayerStructure[i]; j++) {
+					hiddenNodes.get(counter).setGain(g[counter]);
+					counter++;
+				}
+	 		}
+ 		}
+ 		for (int i = 0; i < numOfOutput; i++) {
+ 			outputs.get(i).setGain(g[counter]);
+ 			counter++;
+ 		}
+ 	}
+ 	public void setTime(double t[]){
+ 		int counter = 0;
+ 		if(hiddenLayerUsed){
+	 		for (int i = 0; i < hiddenLayerStructure.length; i++) {
+	 			for (int j = 0; j < hiddenLayerStructure[i]; j++) {
+					hiddenNodes.get(counter).setTime(t[counter]);
+					counter++;
+				}
+	 		}
+ 		}
+ 		for (int i = 0; i < numOfOutput; i++) {
+ 			outputs.get(i).setTime(t[counter]);
+ 			counter++;
+ 		}
+ 	}
 	
 	
 	public void setWeight(double v[]){
@@ -138,10 +165,15 @@ public class ANN {
 						end = start+numOfInput;
 					}
 					else
-						end = start+hiddenLayerStructure[i-1];
-//					if(i == 0)
-//						hiddenNodes.get(counter).setInWeight(Arrays.copyOfRange(v, start, end));
-//					else
+						end = start+hiddenLayerStructure[i-1]+hiddenLayerStructure[i];
+					if(i == 0){
+						hiddenNodes.get(counter).setInWeight(Arrays.copyOfRange(v, start, end));
+						start = end;
+						end = end+hiddenLayerStructure[i]; //self and neighbour weight
+						hiddenNodes.get(counter).setWeight(Arrays.copyOfRange(v, start, end));
+					
+					}
+					else
 						hiddenNodes.get(counter).setWeight(Arrays.copyOfRange(v, start, end));
 					counter++;
 					
@@ -150,60 +182,82 @@ public class ANN {
 			
 			}
 			for (int i = hiddenNodes.size(); i < hiddenNodes.size()+numOfOutput; i++) {
-				outputs.get(i-hiddenNodes.size()).setWeight(Arrays.copyOfRange(v, end, end+hiddenLayerStructure[hiddenLayerStructure.length-1]));
-				end+=hiddenLayerStructure[hiddenLayerStructure.length-1];
+				outputs.get(i-hiddenNodes.size()).setWeight(Arrays.copyOfRange(v, end, end+hiddenLayerStructure[hiddenLayerStructure.length-1]+outputs.size()));
+				end+=hiddenLayerStructure[hiddenLayerStructure.length-1]+outputs.size();
 			}
 		}
 			
 		else{
 			for (int i = 0; i < outputs.size(); i++) {
+//				outputs.get(i-hiddenNodes.size()).setWeight(Arrays.copyOfRange(v, end, end+hiddenLayerStructure[hiddenLayerStructure.length-1]+outputs.size()));
+//				end+=hiddenLayerStructure[hiddenLayerStructure.length-1]+outputs.size();
 				outputs.get(i).setWeight(Arrays.copyOfRange(v, end, end+numOfInput));
 				end+=numOfInput;
 			}
 		}
 		
-		//setting neighbour weights and selfweight:
-		counter = 0;
-		if(hiddenLayerUsed){
-			for (int i = 0; i < hiddenLayerStructure.length; i++) {
-				for (int j = 0; j < hiddenLayerStructure[i]; j++) {
-//					hiddenNodes.get(counter).;
-					counter++;
-					end+=hiddenLayerStructure[i]*hiddenLayerStructure[i];
-				}
-			}
-		}
+		//setting time gains and bias
+		int numOfNodes = getNumberOfNodes();
+		
+			setBias(Arrays.copyOfRange(v, end, end+numOfNodes));
+			end+= numOfNodes;
+			setGains(Arrays.copyOfRange(v, end, end+numOfNodes));
+			end+= numOfNodes;
+			setTime(Arrays.copyOfRange(v, end, end+numOfNodes));
+		
 		
 		
 	}
 	
 	public void run(double[] input){
-		double firstOut[] = new double[numOfInput];
 		if(hiddenLayerUsed){
 			double[] currentOut = input.clone();
 			int counter = 0;
 			for (int i = 0; i < hiddenLayerStructure.length; i++) {
-				double[] newLayer = new double[hiddenLayerStructure[i]];
-				for (int j = 0; j < hiddenLayerStructure[i]; j++) {				
-					
-					hiddenNodes.get(counter).input(currentOut);
-					newLayer[j] = hiddenNodes.get(counter).output(); 
-					counter++;
-//					System.out.println(counter + "/ "+ i + "..." + Arrays.toString(Arrays.copyOfRange(v, start, end)));
-					System.out.println("Hidden layers output:" + Arrays.toString(currentOut));
+				double[] lastOut = new double[hiddenLayerStructure[i]];
+				double[] thisOut = null;
+				if(i > 0)
+					thisOut = new double[hiddenLayerStructure[i-1]];
+				for (int j = 0; j < outputs.size(); j++) {
+					lastOut[j] = outputs.get(j).lastOutput();
 				}
-				currentOut = newLayer;
+				
+				
+				for (int j = 0; j < hiddenLayerStructure[i]; j++) {
+					if(i == 0){
+						hiddenNodes.get(counter).directIn(input.clone()); // direct input
+						hiddenNodes.get(counter).input(lastOut);
+					}
+					else{
+						hiddenNodes.get(counter).input(currentOut);
+						thisOut[j] = hiddenNodes.get(counter).output(); //hentet alle outputene for neste iterasjon 
+						
+					}
+					
+					
+					counter++;
+					
+//					hiddenNodes.get(counter).input(currentOut);					
+				}
+				currentOut = new double[thisOut.length+lastOut.length];
+				for (int j = 0; j < thisOut.length+lastOut.length; j++) {
+					if(j < thisOut.length)
+						currentOut[j] = thisOut[j];
+					else
+						currentOut[j] = lastOut[j-thisOut.length];
+				}
 			}
-			for (int i = 0; i < outputs.size(); i++) {
-				outputs.get(i).input(currentOut);
-			}
+			
 		}
-		else{ //no hidden layer, direct mapping.
-//			System.out.println("inputNode's output:" + Arrays.toString(firstOut));
+		else{
+			double[] newLayer = new double[outputs.size()];
 			for (int i = 0; i < outputs.size(); i++) {
-				outputs.get(i).input(firstOut);
+				newLayer[i] = outputs.get(i).lastOutput();
 			}
-//			System.out.println("output:l" + outputs.get(0).output() + "f"+ outputs.get(1).output() + "r" + outputs.get(2).output());
+			for (int j = 0; j < outputs.size(); j++) {
+				outputs.get(j).directIn(input.clone());
+				outputs.get(j).input(newLayer);
+			}
 		}
 	}
 	
@@ -233,6 +287,54 @@ public class ANN {
 	}
 
 
+// 	public int getNumberOfWeightsNeeded(double v[]){
+//		int numOfW = 0;
+//		if(hiddenLayerUsed){
+//			numOfW += numOfInput*hiddenLayerStructure[0];
+//			for (int i = 1; i < hiddenLayerStructure.length; i++) {
+//				numOfW += hiddenLayerStructure[i-1]*hiddenLayerStructure[i];
+//			}
+//			for (int i = 0; i < hiddenLayerStructure.length; i++) {
+//				numOfW += hiddenLayerStructure[i]; //bias weight
+//				numOfW += hiddenLayerStructure[i]*hiddenLayerStructure[i]; //self weight
+//			}
+//			numOfW += numOfOutput*hiddenLayerStructure[hiddenLayerStructure.length-1]+numOfOutput;
+//		}
+//		else{
+//			numOfW = numOfInput*numOfOutput+numOfOutput;
+//		}
+//		numOfW+= numOfOutput*numOfOutput;
+//		
+//		return numOfW;
+//	}
+	
+//	public void runBACKUP(double[] input){
+//		double firstOut[] = new double[numOfInput];
+//		if(hiddenLayerUsed){
+//			double[] currentOut = input.clone();
+//			int counter = 0;
+//			for (int i = 0; i < hiddenLayerStructure.length; i++) {
+//				double[] newLayer = new double[hiddenLayerStructure[i]];
+//				for (int j = 0; j < hiddenLayerStructure[i]; j++) {				
+//					
+//					hiddenNodes.get(counter).input(currentOut);
+//					newLayer[j] = hiddenNodes.get(counter).output(); 
+//					counter++;
+//				}
+//				currentOut = newLayer;
+//			}
+//			for (int i = 0; i < outputs.size(); i++) {
+//				outputs.get(i).input(currentOut);
+//			}
+//		}
+//		else{ //no hidden layer, direct mapping.
+////			System.out.println("inputNode's output:" + Arrays.toString(firstOut));
+//			for (int i = 0; i < outputs.size(); i++) {
+//				outputs.get(i).input(firstOut);
+//			}
+////			System.out.println("output:l" + outputs.get(0).output() + "f"+ outputs.get(1).output() + "r" + outputs.get(2).output());
+//		}
+//	}
 	
 	
 }
--- Beer Agent/src/simpleslickgame/Node.java
+++ Beer Agent/src/simpleslickgame/Node.java
@@ -1,38 +1,68 @@
 package simpleslickgame;
 
+import java.util.ArrayList;
+
 public class Node {
 	private int functionType;
 	private double threshold;
-	private double w[], x[];
-	private double biasW, biasIn;
+	private double x[], xIn[];
+	private ArrayList<Double> w;
+	private double biasW;
 	private boolean inputNode;
-	private double y;
+	private double y, DY, s;
+	private double gain, time;
+	private boolean hasIn;
 	
-	private double inWeight[];
+	private double out, lastOut;
+	
+	private double inWeight[]; //only used for I_j
 	
 	public Node(double t){
 		biasW = 0;
-		biasIn = 0;
 		functionType = 0;
 		inputNode = false;
 		setThreshold(t);
+		hasIn = false;
+		out = 0;
+		lastOut = 0;
 	}
 	
 	public void setThreshold(double t){
 		threshold = t;
 	}
 	
-	public void input(boolean in[]){
-		x = new double[in.length];
-		for (int i = 0; i < in.length; i++) {
-			x[i]=(in[i]?1:0);
+	
+	
+	
+	public void calculateS(){
+		s = 0;
+		for (int i = 0; i < x.length; i++) {
+			s+= x[i]*w.get(i);
 		}
+		
+		if(hasIn){
+			for (int j = 0; j < inWeight[j]; j++) {
+				s+= xIn[j]*inWeight[j];
+				
+			}
+		}
+			
 	}
 	
-	public void input(boolean in){
-		x = new double[1];
-		x[0] = (in?1:0);
+	
+	public void calculateDY(){
+		DY = (1/time) * (-y+s+biasW);
 	}
+	
+	public void calculateY(){
+		y = y +DY;
+	}
+	
+	public void calculateOut(){
+		lastOut = out;
+		out = 1/(Math.exp(-gain*y));
+	}
+	
 	
 	public void input(double in[]){
 		x = new double[in.length];
@@ -45,8 +75,16 @@ public class Node {
 		x = new double[1];
 		x[0] = in;
 	}
+	
+	public void directIn(double in[]){
+		xIn = new double[in.length];
+		for (int i = 0; i < in.length; i++) {
+			xIn[i]= in[i];
+		}
+	}
+	
 	public void biasInput(double biasIn){
-		this.biasIn = biasIn;
+//		this.biasIn = biasIn;
 	}
 	
 	public void setBiasWeight(double biasW){
@@ -54,75 +92,98 @@ public class Node {
 	}
 	
 	public void setWeight(double v[]){
-		w = new double[v.length];
+		w = new ArrayList<>();
 		for (int i = 0; i < v.length; i++) {
-			w[i] = v[i];
+			w.add(v[i]);
 		}
 	}
 	
 	public void setInWeight(double[] inV) {
+		hasIn = true;
 		inWeight = new double[inV.length];
 		for (int i = 0; i < inV.length; i++) {
-			w[i] = inV[i];
-			//System.out.println("weight set to be:" + w[i]);
+			inWeight[i] = inV[i];
 		}
 	}
-	
-
-	
 	
 	public double output(){
-		if(inputNode){
-			return x[0];
-		}
-		return function(sum());
+		calculateS();
+		calculateDY();
+		calculateY();
+		calculateOut();
+		return out;	
+	}
+	public double lastOutput(){
+		return lastOut;
 	}
 	
-	public double function(double sum){
-		if(functionType == 0){//step
-			if(sum >= threshold)
-				return 1;
-			else if(sum <= -threshold)
-				return -1;
-		}
-		else if(functionType == 1){//sigmoid
-			double sigmoid = 1.0/(1+Math.exp(-sum));
-			return sigmoid;
-		}
-		else if(functionType == 2){//hyporbolicus something tanh stuff
-			double htan = (Math.exp(2*sum)-1)/(Math.exp(2*sum)-1);
-			return htan;
-		}
-		
-		return 0;
-		
-		
-	}
+//	public double function(double sum){
+//		if(functionType == 0){//step
+//			if(sum >= threshold)
+//				return 1;
+//			else if(sum <= -threshold)
+//				return -1;
+//		}
+//		else if(functionType == 1){//sigmoid
+//			double sigmoid = 1.0/(1+Math.exp(-sum));
+//			return sigmoid;
+//		}
+//		else if(functionType == 2){//hyporbolicus something tanh stuff
+//			double htan = (Math.exp(2*sum)-1)/(Math.exp(2*sum)-1);
+//			return htan;
+//		}
+//		
+//		return 0;
+//		
+//		
+//	}
 	
 	
-	public double sum(){
-		double sum = 0;
-		
-		if(w != null)
-			for (int i = 0; i < x.length; i++) {
-				sum += x[i]*this.w[i];
-			}
-		else
-			for (int i = 0; i < x.length; i++) {
-				sum += x[i];
-			}
-		sum+= biasIn*biasW;
-//		System.out.println("SUM IS"+ sum);
-		return sum;
-	}
+//	public double sum(){
+//		double sum = 0;
+//		
+//		if(w != null)
+//			for (int i = 0; i < x.length; i++) {
+//				sum += x[i]*w.get(i);
+//			}
+//		else
+//			for (int i = 0; i < x.length; i++) {
+//				sum += x[i];
+//			}
+//		//sum+= biasIn*biasW;
+////		System.out.println("SUM IS"+ sum);
+//		return sum;
+//	}
+	
 	
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < w.length; i++) {
-			sb.append(w[i] + "\t");
+		if(hasIn){
+			sb.append("Direct weights:\n");
+			for (int i = 0; i < inWeight.length; i++) {
+				
+				sb.append(inWeight[i] + "\t");
+			}
+			
 		}
+		sb.append("\nneighbour weights: \n");
+		for (int i = 0; i < w.size(); i++) {
+			sb.append(w.get(i) + "\t");
+		}
+		//sb.append(biasW);
 		sb.append("\n");
+		sb.append("bias:" +biasW);
+		sb.append("\tgain:" + gain);
+		sb.append("\ttime:" +time+"\n");
 		return sb.toString();
+	}
+
+	public void setGain(double g) {
+		gain = g;
+	}
+	
+	public void setTime(double t){
+		time = t;
 	}
 
 }
